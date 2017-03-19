26960dd89013e1723ef88ae745a1817f55ecb6fc
Yiming Zhang <steven.y.zh@hotmail.com>
2017-03-18 19:57:12 -0700

    Updated the plot.py script

--- scripts/parser/policy/plot.py
+++ scripts/parser/policy/plot.py
@@ -18,13 +18,15 @@ for name in score_dict:
 x_name[1] = "7-Zip"
 
 N = len(x_name) # this is the number of 
-#print(x_name)
+
+#latt, ms, less is better
 latt_p = [] # the policies of latt
 latt_score = [] # the scores of latt
 for item in score_dict['Latt']:
     latt_p.append(item[1])
     latt_score.append(int(item[2]))
 
+#c-ray, s, less is better
 cray_p = [] # the policies of C-Ray
 cray_score = [] # the scores of C-Ray
 for item in score_dict['C-Ray']:
@@ -32,6 +34,7 @@ for item in score_dict['C-Ray']:
     cray_p.append(item[1])
     cray_score.append(float(item[2]))
 
+#7-zip, MIPS, more is better
 sevenzip_p = [] # the policies of 7-zip
 sevenzip_score = [] # the scores of 7-zip
 for item in score_dict['7-Zip Compression']:
@@ -39,7 +42,7 @@ for item in score_dict['7-Zip Compression']:
     sevenzip_p.append(item[1])
     sevenzip_score.append(int(item[2]))
 
-# hackbench has 4 configs
+# hackbench has 4 configs, s, less is better
 hackbench_c = [] # configs of Hackbench
 hackbench_p = [] # the policies of Hackbench
 hackbench_score = [] # the scores of Hackbench
@@ -50,8 +53,8 @@ for i, item in enumerate(score_dict['Hackbench']):
     hackbench_p.append(item[1])
     hackbench_score.append(float(item[2]))
 #print(hackbench_c)
-#print(score_dict['Hackbench'])
-print(hackbench_score)
+
+#ffmpeg, s, less is better
 ff_p = [] # the policies of FFmpeg
 ff_score = [] # the scores of FFmpeg
 for item in score_dict['FFmpeg']:
@@ -59,7 +62,7 @@ for item in score_dict['FFmpeg']:
     ff_p.append(item[1])
     ff_score.append(float(item[2]))
 
-#john the ripper has 3 configs
+#john the ripper has 3 configs, REAL C/S, more is better
 jtr_c = [] # the configs of John The Ripper
 jtr_p = [] # the policies of John The Ripper
 jtr_score = [] # the scores of John The Ripper
@@ -75,13 +78,13 @@ for i, item in enumerate(score_dict['John The Ripper']):
 # modify x_name with configs
 #print(x_name)
 x_name[2], x_name[3] = x_name[3], x_name[2]
-x_name[3] = "HB1"
-x_name[4] = "JTR1"
-x_name.append("HB3")
-x_name.append("HB4")
-x_name.append("HB2")
-x_name.append("JTR2")
-x_name.append("JTR3")
+x_name[3] = "Hackbench\n(Pipes,Process)"
+x_name[4] = "John The Ripper\n(DES)"
+x_name.append("Hackbench\n(Sockets,Process)")
+x_name.append("Hackbench\n(Sockets,Thread)")
+x_name.append("Hackbench\n(Pipes,Thread)")
+x_name.append("John The Ripper\n(MD5)")
+x_name.append("John The Ripper\n(Blowfish)")
 x_name[7], x_name[4] = x_name[4], x_name[7]
 #print(x_name)
 
@@ -91,25 +94,25 @@ num_config = len(latt_score)
 #print(num_config)
 for i in range(0,num_config):
     temp_list = []
-    temp_list.append(math.log2(latt_score[i]))
-    temp_list.append(math.log2(sevenzip_score[i]))
-    temp_list.append(math.log2(ff_score[i]))
-    temp_list.append(math.log2(hackbench_score[i]))
-    temp_list.append(math.log2(jtr_score[i]))
-    temp_list.append(math.log2(hackbench_score[i+14]))
-    temp_list.append(math.log2(hackbench_score[i+21]))
-    temp_list.append(math.log2(hackbench_score[i+7]))
-    temp_list.append(math.log2(jtr_score[i+7]))
-    temp_list.append(math.log2(jtr_score[i+14]))
+    temp_list.append(math.log(latt_score[i],2))
+    temp_list.append(math.log(sevenzip_score[i],2))
+    temp_list.append(math.log(ff_score[i],2))
+    temp_list.append(math.log(hackbench_score[i],2))
+    temp_list.append(math.log(jtr_score[i],2))
+    temp_list.append(math.log(hackbench_score[i+14],2))
+    temp_list.append(math.log(hackbench_score[i+21],2))
+    temp_list.append(math.log(hackbench_score[i+7],2))
+    temp_list.append(math.log(jtr_score[i+7],2))
+    temp_list.append(math.log(jtr_score[i+14],2))
     temp_list[4], temp_list[7] = temp_list[7], temp_list[4]
     conf_list1.append(temp_list)
-print(conf_list1)
-print(len(conf_list1))
+#print(conf_list1)
+#print(len(conf_list1))
 
 # plotting
 #ind = np.arange(N+5)  # the x locations for the groups
 ind =  np.array([0,2,4,6,8,10,12,14,16,18])
-print(ind)
+#print(ind)
 width = 0.15      # the width of the bars
 
 fig, ax = plt.subplots()
@@ -118,16 +121,17 @@ for i in range(0,num_config):
 
 
 # add some text for labels, title and axes ticks
-ax.set_ylabel('Benchmark Scores(log2)')
-ax.set_title('Benchmark Tools')
+ax.set_ylabel('Benchmark Scores')
+ax.set_title('scheduler-comparison')
 ax.set_xticks(ind + width / (N+5))
 #ax.set_xticklabels(('Latt', 'C-Ray', '7-Zip', 'Hackbench', 'FFmpeg', 'John The Ripper'))
-ax.set_xticklabels(x_name)
+ax.set_xticklabels(x_name,size='x-small',stretch='extra-condensed', rotation=20, ha='right')
 #ax.legend()
+ax.set_yscale('log',nonposy='clip')
 ax.legend(loc='best')
 #ax.legend((rects1[0], rects2[0], rects3[0], rects4[0], rects5[0]), ('b1', 'p2', 'p3', 'p4', 'p5'))
 
-'''
+
 def autolabel(rects):
     """
     Attach a text label above each bar displaying its height
@@ -140,7 +144,7 @@ def autolabel(rects):
 
 autolabel(rects1)
 autolabel(rects2)
-'''
+
 
 plt.show()
 
--- scripts/parser/policy/plot2.py
+++ scripts/parser/policy/plot2.py
@@ -4,39 +4,13 @@ XML parsing script for current, cpu usage, memory usage, temperature
 For class project 221 2017 winter
 
 '''
-import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import numpy as np
+import math
+from retrive_info import *
 
-'''
-#tree = ET.parse('latt-test.xml')
-tree = ET.parse('phoronix-results.xml')
-root = tree.getroot()
-
-# Simple data to display in various forms
-x = np.linspace(0, 2 * np.pi, 400)
-y = np.sin(x ** 2)
-
-# Four axes, returned as a 2-d array
-f, axarr = plt.subplots(2, 2)
-axarr[0, 0].plot(x, y)
-axarr[0, 0].set_title('current')
-axarr[0, 1].scatter(x, y)
-axarr[0, 1].set_title('CPU usage')
-axarr[1, 0].plot(x, y ** 2)
-axarr[1, 0].set_title('memory usage')
-axarr[1, 1].scatter(x, y ** 2)
-axarr[1, 1].set_title('default')
-# Fine-tune figure; hide x ticks for top plots and y ticks for right plots
-plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
-plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
-plt.show()
-'''
-
-test = np.array([1,2,3,4,5,6,5,4,3,5,5,])
-t2 = np.array([2,2,6,4,5,6,5,4,3])
-
+print(bm_dict['cpu']['Latt'])
 fig = plt.figure(figsize=(10, 8))
 
 sub1 = fig.add_subplot(221) # equivalent to: plt.subplot(2, 2, 1)
