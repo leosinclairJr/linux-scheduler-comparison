3fec3face6795e2cb476abc1fc2a98f67f6c5598
Yiming Zhang <steven.y.zh@hotmail.com>
2017-03-20 11:10:49 -0700

    Updated plot2.py and fixed all the errors.
    Images are all generated

--- scripts/parser/policy/plot2.py
+++ scripts/parser/policy/plot2.py
@@ -24,13 +24,21 @@ sub1.set_title('CPU usage')
 sub1_policy = []
 cpu_mean = []
 cpu_std = []
+'''
 for i,v in enumerate(bm_dict['cpu']['FFmpeg']):
 	sub1_policy.append(v[1])
 	cpu_mean.append(round(statistics.mean(v[2]),2))
 	cpu_std.append(round(statistics.stdev(v[2]),2))
 	sub1.plot(v[2],color=cmaps[i],label=sub1_policy[i])
-#sub1.legend()
-print(sub1_policy)
+'''
+
+# Hackbench and John The Ripper
+for i in range(14,21):
+	#print(bm_dict['cpu']['Hackbench'][i][2])
+	sub1_policy.append(bm_dict['cpu']['John The Ripper'][i][1])
+	sub1.plot(bm_dict['cpu']['John The Ripper'][i][2],color=cmaps[i-14],label=sub1_policy[i-14])
+
+
 sub2 = plt.subplot(122)
 sub2.set_title('memory usage')
 
@@ -38,12 +46,21 @@ sub2_policy = []
 mm_mean = []
 mm_std = []
 plt2 = []
+
+# Hackbench and John The Ripper
+for i in range(14,21):
+	#print(bm_dict['cpu']['Hackbench'][i][2])
+	sub2_policy.append(bm_dict['memory']['John The Ripper'][i][1])
+	plotobj = sub2.plot(bm_dict['memory']['John The Ripper'][i][2],color=cmaps[i-14],label=sub2_policy[i-14])
+	plt2.append(plotobj[0])
+'''
 for i,v in enumerate(bm_dict['memory']['FFmpeg']):
 	sub2_policy.append(v[1])
 	mm_mean.append(round(statistics.mean(v[2]),2))
 	mm_std.append(round(statistics.stdev(v[2]),2))
 	plotobj = sub2.plot(v[2],color=cmaps[i],label=sub2_policy[i])
 	plt2.append(plotobj[0])
+'''
 #sub1.set_xticklabels(sub1_policy,size='x-small',stretch='extra-condensed', rotation=20, ha='right')
 sub2.legend(bbox_to_anchor=(1, 1.02))
 
