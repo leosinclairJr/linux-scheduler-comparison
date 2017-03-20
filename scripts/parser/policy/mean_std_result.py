53dba40a30e8dc53ef97d323a660e1e1522959c8
Yiming Zhang <steven.y.zh@hotmail.com>
2017-03-20 10:30:39 -0700

    The mean_std_result.json is updated

--- scripts/parser/policy/mean_std_result.py
+++ scripts/parser/policy/mean_std_result.py
@@ -16,23 +16,79 @@ mean_std_result = {}
 for i,v in enumerate(bm_dict['cpu']):
 	cpu_mean = []
 	cpu_std = []
-	if v == 'Hackbench':
-		
+	if v == 'Hackbench': 
+		con_mean1,con_mean2,con_mean3,con_mean4 = [],[],[],[]
+		con_std1,con_std2,con_std3,con_std4 = [],[],[],[]
+		for n in range(0,7):
+			con_mean1.append(round(statistics.mean(bm_dict['cpu'][v][n][2]),2))
+			con_std1.append(round(statistics.stdev(bm_dict['cpu'][v][n][2]),2))
+		for n in range(7,14):
+			con_mean2.append(round(statistics.mean(bm_dict['cpu'][v][n][2]),2))
+			con_std2.append(round(statistics.stdev(bm_dict['cpu'][v][n][2]),2))
+		for n in range(14,21):
+			con_mean3.append(round(statistics.mean(bm_dict['cpu'][v][n][2]),2))
+			con_std3.append(round(statistics.stdev(bm_dict['cpu'][v][n][2]),2))
+		for n in range(21,28):
+			con_mean4.append(round(statistics.mean(bm_dict['cpu'][v][n][2]),2))	
+			con_std4.append(round(statistics.stdev(bm_dict['cpu'][v][n][2]),2))		
+		cpu_mean = [con_mean1,con_mean2,con_mean3,con_mean4]
+		cpu_std = [con_std1,con_std2,con_std3,con_std4]
+		#print(cpu_mean)
 	elif v == 'John The Ripper':
-
+		con_jtr_mean1,con_jtr_mean2,con_jtr_mean3 = [],[],[]
+		con_jtr_std1,con_jtr_std2,con_jtr_std3 = [],[],[]
+		for n in range(0,7):
+			con_jtr_mean1.append(round(statistics.mean(bm_dict['cpu'][v][n][2]),2))
+			con_jtr_std1.append(round(statistics.stdev(bm_dict['cpu'][v][n][2]),2))
+		for n in range(7,14):
+			con_jtr_mean2.append(round(statistics.mean(bm_dict['cpu'][v][n][2]),2))
+			con_jtr_std2.append(round(statistics.stdev(bm_dict['cpu'][v][n][2]),2))
+		for n in range(14,21):
+			con_jtr_mean3.append(round(statistics.mean(bm_dict['cpu'][v][n][2]),2))
+			con_jtr_std3.append(round(statistics.stdev(bm_dict['cpu'][v][n][2]),2))
+		cpu_mean = [con_jtr_mean1,con_jtr_mean2,con_jtr_mean3]
+		cpu_std = [con_jtr_std1,con_jtr_std2,con_jtr_std3]
 	else:
 		for j,k in enumerate(bm_dict['cpu'][v]):
 			cpu_mean.append(round(statistics.mean(k[2]),2))
 			cpu_std.append(round(statistics.stdev(k[2]),2))
 	mean_std_result[v] = {"cpu_mean":cpu_mean,"cpu_std":cpu_std, "memory_mean":None,"memory_std":None}
-
+#print(mean_std_result)
 for i,v in enumerate(bm_dict['memory']):
 	mm_mean = []
 	mm_std = []
-	if v == 'Hackbench':
-		
+	if v == 'Hackbench': 
+		con_mean1,con_mean2,con_mean3,con_mean4 = [],[],[],[]
+		con_std1,con_std2,con_std3,con_std4 = [],[],[],[]
+		for n in range(0,7):
+			con_mean1.append(round(statistics.mean(bm_dict['memory'][v][n][2]),2))
+			con_std1.append(round(statistics.stdev(bm_dict['memory'][v][n][2]),2))
+		for n in range(7,14):
+			con_mean2.append(round(statistics.mean(bm_dict['memory'][v][n][2]),2))
+			con_std2.append(round(statistics.stdev(bm_dict['memory'][v][n][2]),2))
+		for n in range(14,21):
+			con_mean3.append(round(statistics.mean(bm_dict['memory'][v][n][2]),2))
+			con_std3.append(round(statistics.stdev(bm_dict['memory'][v][n][2]),2))
+		for n in range(21,28):
+			con_mean4.append(round(statistics.mean(bm_dict['memory'][v][n][2]),2))	
+			con_std4.append(round(statistics.stdev(bm_dict['memory'][v][n][2]),2))		
+		mm_mean = [con_mean1,con_mean2,con_mean3,con_mean4]
+		mm_std = [con_std1,con_std2,con_std3,con_std4]
+		#print(cpu_mean)
 	elif v == 'John The Ripper':
-		
+		con_jtr_mean1,con_jtr_mean2,con_jtr_mean3 = [],[],[]
+		con_jtr_std1,con_jtr_std2,con_jtr_std3 = [],[],[]
+		for n in range(0,7):
+			con_jtr_mean1.append(round(statistics.mean(bm_dict['memory'][v][n][2]),2))
+			con_jtr_std1.append(round(statistics.stdev(bm_dict['memory'][v][n][2]),2))
+		for n in range(7,14):
+			con_jtr_mean2.append(round(statistics.mean(bm_dict['memory'][v][n][2]),2))
+			con_jtr_std2.append(round(statistics.stdev(bm_dict['memory'][v][n][2]),2))
+		for n in range(14,21):
+			con_jtr_mean3.append(round(statistics.mean(bm_dict['memory'][v][n][2]),2))
+			con_jtr_std3.append(round(statistics.stdev(bm_dict['memory'][v][n][2]),2))
+		mm_mean = [con_jtr_mean1,con_jtr_mean2,con_jtr_mean3]
+		mm_std = [con_jtr_std1,con_jtr_std2,con_jtr_std3]
 	else:
 		for j,k in enumerate(bm_dict['memory'][v]):
 			mm_mean.append(round(statistics.mean(k[2]),2))
@@ -44,3 +100,5 @@ for i,v in enumerate(bm_dict['memory']):
 #result.write(mean_std_result)
 with open('mean_std_result.json', 'w') as f:
     json.dump(mean_std_result,f)
+
+
